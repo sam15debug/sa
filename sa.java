9d981e667f00f1365ef232f2e372eea7061a6b25
do0dle_head <71787955+sam15debug@users.noreply.github.com>
2022-07-01 14:30:02 +0530

    Update and rename sa.java to sa.py

--- sa.java
+++
@@ -1,9 +0,0 @@
-public class sa
-{
-public static void main(String args[])
-{
-int n1 = 2 , n2 = 115, sum;
-sum = n1 + n2;
-System.out.println("The sum of numbers is: "+sum);
-}
-}
---
+++ sa.py
@@ -0,0 +1,3 @@
+a = 12
+b = 12
+print("Sum of two numbers is : ", a+b)

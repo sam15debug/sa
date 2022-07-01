672a29d9299e3d3122f4eae8798abf2837d9646f
do0dle_head <71787955+sam15debug@users.noreply.github.com>
2022-07-01 14:07:19 +0530

    Update sa.java

--- sa.java
+++ sa.java
@@ -2,7 +2,7 @@ public class sa
 {
 public static void main(String args[])
 {
-int n1 = 225, n2 = 115, sum;
+int n1 = 2 , n2 = 115, sum;
 sum = n1 + n2;
 System.out.println("The sum of numbers is: "+sum);
 }
